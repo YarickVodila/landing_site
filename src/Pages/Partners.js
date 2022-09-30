38bd900c6c352916e222791fa34656e5323dfb97
YarickVodila <y.koltashev@mail.ru>
2022-09-30 23:43:17 +0500

    first cum it

--- src/Pages/Partners.js
+++ src/Pages/Partners.js
@@ -1,7 +1,10 @@
 import React, {Component } from 'react';
 
-import uralenergotel from "../img/Uralenergotel.jpg";
-import Motiv from "../img/Motiv.jpg";
+import uralenergotel from "../img/uralenergotel.png";
+import Motiv from "../img/motiv.png";
+import energoit from "../img/EnergoIT.png";
+import grchc from "../img/GRCHC.png";
+import iut from "../img/IUT.png";
 
 export default class Partners extends Component{
     render() {
@@ -12,8 +15,41 @@ export default class Partners extends Component{
                         <h1><b>Партнёры</b></h1>
                     </section>
                     <section className={'Logos'}>
-                        <img className={'partners-logo'} src={uralenergotel} width={200} height={200} alt={'Мотив'}/>
-                        <img className={'partners-logo'} src={Motiv} width={200} height={200} alt={'Уралэнерготел'}/>
+                        <section className={'About-partners'}>
+                            <img className={'partners-logo'} src={Motiv} width={250} height={50} alt={'Мотив'}/>
+                            <p>
+                                Телекоммуникационная группа Мотив – ведущий инфраструктурный оператор связи в Уральском
+                                Федеральном округе. Сайт компании https://motivtelecom.ru/
+                            </p>
+                        </section>
+                        <section className={'About-partners'}>
+                            <img className={'partners-logo'} src={iut} width={250} height={50} alt={''}/>
+                            <p>
+                                Телекоммуникационная группа Мотив – ведущий инфраструктурный оператор связи в Уральском
+                                Федеральном округе. Сайт компании https://motivtelecom.ru/
+                            </p>
+                        </section>
+                        <section className={'About-partners'}>
+                            <img className={'partners-logo'} src={uralenergotel} width={100} height={100} alt={'Уралэнерготел'}/>
+                            <p>
+                                Телекоммуникационная группа Мотив – ведущий инфраструктурный оператор связи в Уральском
+                                Федеральном округе. Сайт компании https://motivtelecom.ru/
+                            </p>
+                        </section>
+                        <section className={'About-partners'}>
+                            <img className={'partners-logo'} src={energoit} width={100} height={100} alt={''}/>
+                            <p>
+                                Телекоммуникационная группа Мотив – ведущий инфраструктурный оператор связи в Уральском
+                                Федеральном округе. Сайт компании https://motivtelecom.ru/
+                            </p>
+                        </section>
+                        <section className={'About-partners'}>
+                            <img className={'partners-logo'} src={grchc} width={100} height={100} alt={''}/>
+                            <p>
+                                Телекоммуникационная группа Мотив – ведущий инфраструктурный оператор связи в Уральском
+                                Федеральном округе. Сайт компании https://motivtelecom.ru/
+                            </p>
+                        </section>
                     </section>
                 </section>
 
--- src/Pages/Timeline.js
+++ src/Pages/Timeline.js
@@ -12,7 +12,7 @@ export default class Timeline extends Component{
                         <h1><b>Таймлайн</b></h1>
                     </div>
                     <section className={"time-logo"}>
-                        <img className={'Logo-time'} src={timelogo} width={600} height={450}/>
+                        <img className={'Logo-time'} src={timelogo} width={500} height={450}/>
                     </section>
                 </main>
             </div>
